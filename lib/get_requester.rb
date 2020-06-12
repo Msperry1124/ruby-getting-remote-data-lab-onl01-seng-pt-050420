7ee471c2e6c74cd4d1ed7e87a9cdcd3e7f094a95
Melissa Sperry <sperry.mel@gmail.com>
2020-06-12 01:22:26 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -12,7 +12,7 @@ class GetRequester
   #   response.body
   # end
 
-  GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+  get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
   get_requester.parse_json
 
 
