9e6afc4d7656b6878fd4e679276c351455009a23
Melissa Sperry <sperry.mel@gmail.com>
2020-06-12 01:23:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,11 +7,15 @@ class GetRequester
   get_requester = GetRequester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
   get_requester.parse_json
 
-  # def get_response_body
-  #   uri = URI.parse(URL)
-  #   respone = Net::HTTP.get_response(URL)
-  #   response.body
-  # end
+  def get_response_body
+    uri = URI.parse(URL)
+    respone = Net::HTTP.get_response(URL)
+    response.body
+  end
+
+  def parse_json
+    
+  end
 
 
 
